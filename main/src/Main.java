ba6461ba3e0983acc17de7df195709d0b4234dd4
Haaim <h.syed1@studenti.unibg.it>
2023-04-03 17:17:17 +0200

    Generated test players to ensure Leaderboard worked

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,7 +1,21 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args){
-        //test object
-        Player player1 = new Player(1, "Giorgio");
-        System.out.println(player1.getName());
+        Scanner sc = new Scanner(System.in);
+        int numPlayers = 0;
+        System.out.println("quanti giocatori stanno giocando? (non meno di 2 o più di 4)");
+        while(numPlayers < 2 || numPlayers > 4){
+        numPlayers = sc.nextInt();
+        }
+        Player[] giocatori = Player.GeneratePlayers(numPlayers);
+        for(Player pl : giocatori)
+            System.out.println(pl.getName());
+
+        // setting dei punti temporaneo, da rimuovere
+        giocatori[2].setPoints(32);
+        giocatori[1].setPoints(43);
+
+        Game.Leaderboard(giocatori);
+
     }
 }
