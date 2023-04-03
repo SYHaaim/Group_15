9525c6b749f685a11527c57884d478f8ce3e262a
Haaim <h.syed1@studenti.unibg.it>
2023-04-03 17:16:31 +0200

    added "Leaderboard" method

--- main/src/Game.java
+++ main/src/Game.java
@@ -1,3 +1,9 @@
 public class Game {
     //handles player turns and endgame etc..
+
+    public static void Leaderboard(Player[] players){
+        Player.SortByPoints(players);
+        for(int i = 0; i < players.length; i++)
+            System.out.println((i+1)+". "+ players[i].getName() + " punti: " + players[i].getPoints());
+    }
 }
