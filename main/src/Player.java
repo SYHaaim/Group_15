2713554b5d52819d890466754f12fdfa499bc215
Haaim <h.syed1@studenti.unibg.it>
2023-04-03 17:18:12 +0200

    added "GeneratePlayers" and "SortByPoints" methods

--- main/src/Player.java
+++ main/src/Player.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Player {
 
     //player class, handles player generation, points and personal objectives
@@ -11,9 +13,37 @@ public class Player {
         return name;
     }
 
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
     public Player(int id, String name) {
         this.name = name;
         this.id = id;
+        this.points = 0;
+    }
 
+    public static Player[] GeneratePlayers(int numPlayers){
+        Scanner sc = new Scanner(System.in);
+        Player[] players = new Player[numPlayers];
+        for (int i = 0; i < players.length; i ++){
+            System.out.println("come si chiama il " + (i+1) + "° giocatore?");
+            players[i] = new Player(i+1, sc.next());
+        }
+        return players;
+    }
+
+    public static void SortByPoints(Player[] players){
+        for (int i = 0; i < players.length-1; i++){
+            if (players[i].points < players[i+1].points){
+                Player tmp = players[i+1];
+                players[i+1] = players[i];
+                players[i]=tmp;
+            }
+        }
     }
 }
