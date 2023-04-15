c2827e4225159d18c8a5f96244884afe1fe2bd08
Haaim <h.syed1@studenti.unibg.it>
2023-04-15 19:14:31 +0200

    added method that handles player turn, temporary action to be replaced as of now

--- main/src/Game.java
+++ main/src/Game.java
@@ -1,9 +1,20 @@
+import java.util.*;
+
 public class Game {
     //handles player turns and endgame etc..
 
-    public static void Leaderboard(Player[] players){
+    public static void PrintLeaderboard(Player[] players){
         Player.SortByPoints(players);
         for(int i = 0; i < players.length; i++)
             System.out.println((i+1)+". "+ players[i].getName() + " punti: " + players[i].getPoints());
+    }
+
+    public static void PlayerTurn(Player pl){
+        //temporary action, to replace with picking from 1 to 3 tiles
+        String action = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("è il turno di " + pl.getName() + ", Indica una mossa");
+        action = sc.next();
+        pl.setActionString(action);
     }
 }
