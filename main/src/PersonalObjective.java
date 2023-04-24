be32ee3db6f0b7cc7bb74ed1954ae24be10af7c1
Haaim <h.syed1@studenti.unibg.it>
2023-04-24 21:48:49 +0200

    generatePersonal now concatenates a random tile type in the PersonalObjective formatting

--- main/src/PersonalObjective.java
+++ main/src/PersonalObjective.java
@@ -1,23 +1,27 @@
+
+
+
 import java.util.Random;
 
 public class PersonalObjective{
 
-	private String[] objectives;
+	private final String[] objectives;
 
 	public PersonalObjective(){
 		this.objectives = new String[6];
 		generatePersonal();
 	}
 
-	//da aggiungere il tipo della tessera nella formattazione degli obbiettivi: "x coord. - y coord. - tile type"
+	//"x coord. - y coord. - tile type"
 	public void generatePersonal(){
-		String toAdd = " ";
+		String toAdd;
 		Random r = new Random();
 
 		for (int i = 0; i < objectives.length; i++){
 			toAdd = (r.nextInt(5) + 1) + " - " + (r.nextInt(6)+1);
 			objectives[i] = toAdd;
 		}
+		TileType.concatenateToString(objectives);
 	}
 
 	public void printObj(){
