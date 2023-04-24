7e9a1f513e7e2626922e63aee1755cb3b4a3a1f1
Haaim <h.syed1@studenti.unibg.it>
2023-04-24 21:46:42 +0200

    added methods to concatenate random tile types while generating PersonalObjective

--- main/src/TileType.java
+++ main/src/TileType.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public enum TileType {
     C("CATS"),
     B("BOOKS"),
@@ -7,7 +11,53 @@ public enum TileType {
     P("PLANTS");
 
     public final String tileName;
-    private TileType(String tileName){
+     TileType(String tileName){
         this.tileName = tileName;
+    }
+
+    public static void concatenateToString(String[] objvs){
+        Random r = new Random();
+
+        //randInts contains 6 unique integers to generate 6 unique tile types in random order
+        ArrayList<Integer> randInts = new ArrayList<>();
+
+        while (randInts.size() < 6) {
+            int a = r.nextInt(6)+1;
+
+            if (!randInts.contains(a)) {
+                randInts.add(a);
+            }
+        }
+
+        for(int i = 0; i < objvs.length; i++){
+            objvs[i] += " - " + generateRandomType(randInts.get(i));
+        }
+    }
+    private static String generateRandomType(int cases){
+
+        String result = "";
+        switch (cases){
+            case 1:
+                result = "C";
+                break;
+            case 2:
+                result = "B";
+                break;
+            case 3:
+                result = "T";
+                break;
+            case 4:
+                result = "F";
+                break;
+            case 5:
+                result = "TR";
+                break;
+            case 6:
+                result = "P";
+                break;
+
+        }
+
+        return result;
     }
 }
