5aba29e1008c8ad33157e7e26a98c8ed3c386351
Haaim <h.syed1@studenti.unibg.it>
2023-05-03 19:06:37 +0200

    playerTurn now makes you pick up to 3 tiles, added method to check that a tile is within board limits

--- main/src/Game.java
+++ main/src/Game.java
@@ -10,11 +10,74 @@ public class Game {
     }
 
     public static void PlayerTurn(Player pl){
-        //temporary action, to replace with picking from 1 to 3 tiles
-        String action = "";
+        int column = 0;
+        int pickedCount = 0;
+        char row = ' ';
+        boolean pickMore = true;
         Scanner sc = new Scanner(System.in);
-        System.out.println("è il turno di " + pl.getName() + ", Indica una mossa");
-        action = sc.next();
-        pl.setActionString(action);
+
+        System.out.println("TURNO DI: " + pl.getName());
+        while(pickMore){
+
+            row = provideValidInput(row);
+            column = provideValidInput(column);
+
+            System.out.println("vuoi prendere altre caselle? (si/no)");
+            if (sc.next().equalsIgnoreCase("NO")){
+                pickMore = false;
+            }
+
+            pl.pickTiles(row, column, pickedCount);
+
+            pickedCount++;
+            if(pickedCount > 2)
+                pickMore = false;
+        }
+
+    }
+
+
+    private static char provideValidInput(char in){
+        boolean isValid = false;
+        Scanner sc = new Scanner(System.in);
+
+        while(!isValid){
+
+            System.out.println("Scegli la riga da cui prendere una casella (A - I)");
+            try {
+                in = sc.next().toUpperCase().charAt(0);
+                char startRow = 'A';
+                char endRow = 'I';
+                if (Character.getNumericValue(in) >= (Character.getNumericValue(startRow)) && Character.getNumericValue(in) <= (Character.getNumericValue(endRow))) {
+                    isValid = true;
+                } else {
+                    System.out.println("input invalido. scegli una lettera tra A ed I");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("input invalido. scegli una lettera tra A ed I");
+                sc.nextLine(); // consume the remaining input
+            }
+        }
+        return in;
+    }
+    private static int provideValidInput(int in){
+        boolean isValid = false;
+        Scanner sc = new Scanner(System.in);
+        while(!isValid){
+
+            System.out.println("Scegli la colonna da cui prendere una casella (1 - 9)");
+            try {
+                in = sc.nextInt();
+                if (in >= 1 && in <= 9) {
+                    isValid = true;
+                } else {
+                    System.out.println("input invalido. scegli un numero tra 1 e 9");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("input invalido. scegli un numero tra 1 e 9");
+                sc.nextLine(); // consume the remaining input
+            }
+        }
+        return in;
     }
 }
