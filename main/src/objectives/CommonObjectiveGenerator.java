4d85318fba62b5c6e0ac39cf40e6827eb6d52dc0
lorenzomedicii <l.medici2@studenti.unibg.it>
2023-05-24 12:40:46 +0200

    sistemate variabili e iniziato l'estrazione random dei goal

--- main/src/objectives/CommonObjectiveGenerator.java
+++ main/src/objectives/CommonObjectiveGenerator.java
@@ -3,19 +3,29 @@ package objectives;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
+import java.math.*;
 
 public class CommonObjectiveGenerator {
-    private static final ArrayList<String[]> TessereObbComuni = new ArrayList<>();
+    //private static final ArrayList<String[]> TessereObbComuni = new ArrayList<>();
 
-    private void readFromFile() throws FileNotFoundException {
+    private String[] readFromFile() throws FileNotFoundException {
     	String line = null;
-		String[] campi = new String[10];
+		String[] goals = new String[10];
 		Scanner scdescription=new Scanner(new File("description.txt"));             
 		int i = 0;
 		while(scdescription.hasNext()) {                 
 		line =scdescription.nextLine();                 
-		campi[i]= line;
+		goals[i]= line;
 		i++;
 		}
+		return goals;
     }
+    
+   // private String[] randomGoal(int nPlayer) {
+    	
+    	//int prova = (int) (Math.random() * nPlayer);
+    
+    //}
 }
+
