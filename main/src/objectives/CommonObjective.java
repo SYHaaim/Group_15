76f6bcbd71f2b35f6d3453b46d01deb96fe69ecd
Haaim <h.syed1@studenti.unibg.it>
2023-05-24 16:01:16 +0200

    fixed file reading and laid structure for common objectives controls

--- main/src/objectives/CommonObjective.java
+++ main/src/objectives/CommonObjective.java
@@ -1,12 +1,42 @@
 package objectives;
 
+import java.io.FileNotFoundException;
+import library.Library;
+
 public class CommonObjective{
 
-		int cardId;
-		String descrizione;
- 		public CommonObjective(){
+		private int cardId;
+		private String descrizione;
+		private CommonObjectiveGenerator generator;
+		private int[] objectivePoiints = {8,4,6,2};
+
+ 		public CommonObjective() throws FileNotFoundException {
+			this.generator = new CommonObjectiveGenerator();
+			this.descrizione = generator.getGeneratedObjective();
+			this.cardId = generator.getGeneratedId();
 
 		}
+
+		public void printCommonObj(){
+			System.out.println(this.cardId +" - "+this.descrizione);
+		}
+
+		public void checkCommonObjectives(int objectiveId, Library libreria){
+			 //gli id sono progressivi rispettivamente il loro ordinamento nel file "description.txt"
+			switch (objectiveId){
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+				case 7:
+				case 8:
+				case 9:
+				case 10:
+			}
+		}
+
 	
 }
 	               
--- main/src/objectives/CommonObjectiveGenerator.java
+++ main/src/objectives/CommonObjectiveGenerator.java
@@ -9,16 +9,34 @@ import java.math.*;
 public class CommonObjectiveGenerator {
     //private static final ArrayList<String[]> TessereObbComuni = new ArrayList<>();
 
-    private String[] readFromFile() throws FileNotFoundException {
+	private String generatedObjective;
+	private int generatedId;
+
+	public CommonObjectiveGenerator() throws FileNotFoundException {
+		generatedObjective = readFromFile();
+	}
+
+	public String getGeneratedObjective() {
+		return generatedObjective;
+	}
+
+	public int getGeneratedId() {
+		return generatedId;
+	}
+
+	private String readFromFile() throws FileNotFoundException {
     	String line = null;
-		String[] goals = new String[10];
+		String goals;
+		Random r = new Random();
 		Scanner scdescription=new Scanner(new File("description.txt"));             
 		int i = 0;
 		while(scdescription.hasNext()) {                 
-		line =scdescription.nextLine();                 
-		goals[i]= line;
-		i++;
+			line += scdescription.nextLine();
 		}
+		assert line != null;
+		String[] objs = line.split("-");
+		this.generatedId = r.nextInt(9)+1;
+		goals = objs[this.generatedId];
 		return goals;
     }
     
